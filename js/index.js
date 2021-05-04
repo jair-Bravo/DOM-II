38cad9188e3fbae5fa349197de02500d29c92abb
unknown <jair_iosif@live.com>
2021-05-04 18:19:57 -0500

    added two events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+
+mountainExcursion.addEventListener('mouseover' , function(event) {
+    mountainExcursion.style.backgroundColor = ('blue');
+    mountainExcursion.style.borderRadius = ('10%');
+});
+
+
+islandGetAway.addEventListener ('mouseover', function(event) {
+    islandGetAway.style.backgroundColor = ('red');
+    islandGetAway.style.borderRadius = ('10%');
+});
