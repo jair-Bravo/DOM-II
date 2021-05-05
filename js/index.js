364b23ed02906b50b610276e0b44989be785af8b
unknown <jair_iosif@live.com>
2021-05-04 23:20:58 -0500

    created event listeners

--- js/index.js
+++ js/index.js
@@ -1,10 +1,31 @@
 // Your code goes here
 
 
-mountainExcursion.addEventListener('mouseover' , function(event) {
-    mountainExcursion.style.backgroundColor = ('blue');
-    mountainExcursion.style.borderRadius = ('10%');
-});
+const destination = document.querySelector(".destination")
+const mountainExcursion = document.querySelector(".mountainExcursion")
+const islandGetAway = document.querySelector(".islandGetAway")
+const h4 = document.querySelector("h4")
+const footer = document.querySelector(".footer")
+
+
+destination.addEventListener("click", e => {
+    console.log(e)
+})
+
+dispatchEvent.forEach(div => {
+    div.addEventListener("click", () => {
+        console.log("hi")
+    })
+})
+
+// mountainExcursion.addEventListener("click", e => {
+//     console.log(e)
+// })
+
+// mountainExcursion.addEventListener('mouseover' , function(event) {
+//     mountainExcursion.style.backgroundColor = ('blue');
+//     mountainExcursion.style.borderRadius = ('10%');
+// });
 
 
 islandGetAway.addEventListener ('mouseover', function(event) {
